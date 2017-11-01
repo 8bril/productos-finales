c6616be2c870d62b832ee50f62ddc5b99f22d82a
8bril <mancusoo@gmail.com>
2017-10-31 18:19:29 -0600

    carpeta actualizada

--- productos-finales/cifrado cesar/js/app.js
+++ productos-finales/cifrado cesar/js/app.js
@@ -1,12 +1,12 @@
-var phrase = (prompt('Encipher your phrase')).toUpperCase(); // Pediremos al usuario ingresar una frase 
+var string = (prompt('Encipher your phrase')).toUpperCase(); // Pediremos al usuario ingresar una frase 
 
- if ( /^[a-zA-Z]*$/.test(phrase) === false ) {
+ if ( /^[a-zA-Z]*$/.test(string) === false ) {
   alert ('Write Only Letters');  // si el usuario ingresa un un número o espacio no ingresara al programa
   }
 
-function caesarCipher(phrase){
+function caesarCipher(string){
   var fillArray = []; // implementamos el array vacio a recorrer
-  var strPhrase = phrase.split(''); // nos devolvera el array con sus elementos
+  var strPhrase = string.split(''); // nos devolvera el array con sus elementos
 
   for( var i = 0; i < strPhrase.length; i++){
       fillArray.push(String.fromCharCode((((strPhrase[i].charCodeAt()-65)+33)%26)+65)); 
@@ -19,4 +19,4 @@ function caesarCipher(phrase){
   return result; //  
  }
 
-  document.write('Your result of your phrase is ' + caesarCipher(phrase)); // imprime el resultado de la frase cifrada en pantalla
+  document.write('Your result of your phrase is ' + caesarCipher(string)); // imprime el resultado de la frase cifrada en pantalla
